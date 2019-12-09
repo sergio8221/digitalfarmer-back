893b9cf14f1fe49944148c44c9e6f898526557ef
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2019-12-09 22:47:31 +0100

    Spring Boot project initialization

---
+++ digitalfarmer/src/main/java/com/sergiogutierrez/digitalfarmer/DigitalfarmerApplication.java
@@ -0,0 +1,13 @@
+package com.sergiogutierrez.digitalfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalfarmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalfarmerApplication.class, args);
+	}
+
+}
---
+++ digitalfarmer/src/test/java/com/sergiogutierrez/digitalfarmer/DigitalfarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiogutierrez.digitalfarmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalfarmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
